42ace954956c44088d492ccb2acde9514875c3f8
MukeshRawatMkR <swissmukeshrawat@gmail.com>
2024-05-18 18:56:01 +0530

    implemented Link componenet

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ import Body from "./components/Body";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet/>
+
+
     </div>
   );
 };
@@ -19,16 +21,23 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-  errorElement:<Error/>,
+    children:[
+      {
+        path:"/",
+        element:<Body/>
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement:<Error/>,
   },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
+  
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,40 @@
-import { LOGO_URL} from "../utils/constants";
+import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const[btnNameReact, setBtnNameReact]=useState("Login");//local state variable.
+  const [btnNameReact, setBtnNameReact] = useState("Login"); //local state variable.
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src={LOGO_URL}
-        />
+        <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>  
+          <li>
+          <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact"> Contact Us</Link>
+          </li>
           <li>Cart</li>
-        <button className="login" onClick={()=>
-        { btnNameReact==="Login"?
-          setBtnNameReact("Logout"):setBtnNameReact("Login");
-      
-        }}
-        >
-        {btnNameReact}
-        </button>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
   );
 };
 
-  export default Header;
+export default Header;
