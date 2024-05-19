15ac46e587fa5958839545fb8d5acd81455a60cc
MukeshRawatMkR <swissmukeshrawat@gmail.com>
2024-05-19 15:22:57 +0530

    started exploring with class based components on my page UserClass

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>Learning React</h2>
+    <User/>
+    <UserClass name={"Mukesh Rawat(class)"} Location={"Lansdowne Class"}/>
     </div>
   );
 };
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User=()=>{
+return(
+    <div className="user-card">
+        <h2>Name: Mukesh </h2>
+        <h3>Location: Lansdowne</h3>
+        <h4>Contact: @rwtposts</h4>
+
+    </div>
+)
+};
+export default User;
