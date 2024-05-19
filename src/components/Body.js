c45bab9771cc79d54572784d263adb9dbeb9c567
MukeshRawatMkR <swissmukeshrawat@gmail.com>
2024-05-20 02:17:03 +0530

    imported tailwind to our project

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,65 +7,86 @@ const Body = () => {
   //Local state variable -> super powerful variable
   const [listOfRestaurants, setListOfRestaurant] = useState([]);
 
-  const[filteredRestaurant, setFilteredRestaurant]=useState([]); 
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  const [searchText, setSearchText]=useState("");
+  const [searchText, setSearchText] = useState("");
 
-useEffect(()=>{
-  // console.log("useEffect Called");
-  fetchData();
-},[]);
+  useEffect(() => {
+    // console.log("useEffect Called");
+    fetchData();
+  }, []);
 
-const fetchData=async ()=>{
-  const data=await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.2685607&lng=78.00710389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  );
-  const json=await data.json();
-  // console.log(json);
-  setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-}
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.2685607&lng=78.00710389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json);
+    setListOfRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-  return listOfRestaurants.length===0?(<Shimmer/>):(//this is ternary operator.
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer /> //this is ternary operator.
+  ) : (
     <div className="body">
-      <div className="filter">
-       <div className="search">
-        <input type="text" className="search-box" value={searchText} 
-        onChange={(e)=>{
-setSearchText(e.target.value);
-        }}/>
-        <button onClick={()=>{
-          //filter the restaurant cards and update the UI.
-          //serachText
-
-        }}>Search</button>
-       </div>
+      <div className="filter flex">
+        <div className="search m-4 p-4 flex items-center">
+          { <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          /> }
+          <button
+            className="px-4 bg-green-100 m-4 rounded-lg"
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().include(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="search m-4 p-4">
         <button
-          className="filter-btn"
+          className="p-2 bg-green-100 m-4 rounded-lg"
           onClick={() => {
             //filter logic here
-          const filteredRestaurant= listOfRestaurants.filter(
-              (res) => res?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.[0]?.info?.avgRating> 4
+            const filteredRestaurant = listOfRestaurants.filter(
+              (res) =>
+                res?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle
+                  ?.restaurants?.[0]?.info?.avgRating > 4
             );
             // console.log(listOfRestaurants);
-          
+
             setFilteredRestaurant(filteredRestaurant);
-      
           }}
         >
           Top Rated Restaurants
         </button>
+        </div>
+        
       </div>
-      <div className="res-container">
+      <div className="res-container flex flex-wrap">
         {
           //whenever we loop through map we should always use key to get rid of warning on console.
           filteredRestaurant.map((restaurant) => (
-            <Link 
-            key={restaurant.info.id}
-            to={"/restaurants/" + restaurant.info.id}> 
-            <RestaurantCard resData={restaurant} />
+            <Link
+              key={restaurant.info.id}
+              to={"/restaurants/" + restaurant.info.id}
+            >
+              <RestaurantCard resData={restaurant} />
             </Link>
-           ))
+          ))
         }
       </div>
     </div>
