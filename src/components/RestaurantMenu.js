eeb2d14fd7f33f974f2dee71c76906d68542fc18
MukeshRawatMkR <swissmukeshrawat@gmail.com>
2024-05-20 10:59:34 +0530

    implemented custom hook

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,31 +1,22 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurant";
 import Shimmer from "./Shimmer";
-import { MENU_API } from "../utils/constants";
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+const{resId}=useParams();
+const resInfo=useRestaurantMenu(resId);//custom hook.
+  if (resInfo === null) return <Shimmer />;
 
-  const fetchMenu = async () => {
-    const data = await fetch(
-      MENU_API
-    );
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const { name, cuisines, costForTwo } =
+    resInfo?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]
+      ?.info;
 
-  if(resInfo === null) return <Shimmer />;
- 
-  const{name, cuisines, costForTwo}  = resInfo?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]?.info;
- 
- 
-    return (
+  return (
     <div className="menu">
       <h1>{name}</h1>
-      <p>{cuisines.join(",")} - {costForTwo}</p>
+      <p>
+        {cuisines.join(",")} - {costForTwo}
+      </p>
       <ul>
         <li>Paneer</li>
         <li>Burgers</li>
